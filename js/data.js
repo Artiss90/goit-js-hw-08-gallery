6523c356c4c145f5b4079676ede3c8a1ac52f56a
Art <Petrenkoartik@gmail.com>
2020-10-12 00:14:40 +0300

    убрал лишние консоли

--- js/data.js
+++ js/data.js
@@ -2,12 +2,12 @@ import dataGalleryItems from './gallery-items.js'
 // console.log(dataGalleryItems);
 
 const refs = {
-    listGalleryItems: document.querySelector('.js-gallery'),
-    lightboxModal: document.querySelector('.js-lightbox'),
-    overlayLightboxModal: document.querySelector('.lightbox__overlay'),
-    contentLightboxModal: document.querySelector('.lightbox__content'),
-    btnLightboxModal: document.querySelector('.lightbox__button'),
-    imgContentLightboxModal: document.querySelector('.lightbox__image')
+  listGalleryItems: document.querySelector('.js-gallery'),
+  lightboxModal: document.querySelector('.js-lightbox'),
+  overlayLightboxModal: document.querySelector('.lightbox__overlay'),
+  contentLightboxModal: document.querySelector('.lightbox__content'),
+  btnLightboxModal: document.querySelector('.lightbox__button'),
+  imgContentLightboxModal: document.querySelector('.lightbox__image')
 }
 // console.log(refs.listGalleryItems);
 // console.log(refs.imgContentLightboxModal);
@@ -18,7 +18,7 @@ refs.listGalleryItems.innerHTML = cardsMarkupGalleryItems;
 
 function createListGalleryItems(items) {
   return items.map(({ preview, original, description }) => {
-      return `
+    return `
    <li class="gallery__item">
   <a
     class="gallery__link"
@@ -33,23 +33,24 @@ function createListGalleryItems(items) {
   </a>
 </li>
     `;
-    })
+  })
     .join('');
 }
 
 refs.listGalleryItems.addEventListener('click', viewOriginalImageGallery)
 
-function viewOriginalImageGallery(e) {e.preventDefault()
-    // console.log('целевой:', e.target);
-    // console.log('текущий:', e.currentTarget);
-     /* если не IMG - пропускаем */
-    if (e.target.nodeName !== 'IMG') { return }
-    const OriginalImageGallery = e.target.dataset.source
-    // console.log(OriginalImageGallery);
+function viewOriginalImageGallery(e) {
+  e.preventDefault()
+  // console.log('целевой:', e.target);
+  // console.log('текущий:', e.currentTarget);
+  /* если не IMG - пропускаем */
+  if (e.target.nodeName !== 'IMG') { return }
+  const OriginalImageGallery = e.target.dataset.source
+  // console.log(OriginalImageGallery);
 
-    addClassForOpenModal()
-    addLinkOriginalImageInModal(OriginalImageGallery)
-    //  console.log('this:', refs.imgContentLightboxModal.getAttribute('src'));
+  addClassForOpenModal()
+  addLinkOriginalImageInModal(OriginalImageGallery)
+  //  console.log('this:', refs.imgContentLightboxModal.getAttribute('src'));
 }
 
 refs.btnLightboxModal.addEventListener('click', addClassForOpenModal)
@@ -57,13 +58,12 @@ refs.overlayLightboxModal.addEventListener('click', addClassForOpenModal)
 // refs.overlayLightboxModal.addEventListener('click', removeLinkOriginalImageInModal)
 
 function addClassForOpenModal(e) {
-    if (refs.lightboxModal.classList.contains("is-open")) {
-        return refs.lightboxModal.classList.remove("is-open"),
-         refs.imgContentLightboxModal.removeAttribute('src'),
-            refs.imgContentLightboxModal.removeAttribute('alt'),console.log( refs.imgContentLightboxModal.getAttribute('src'));
-
-    }
-return refs.lightboxModal.classList.add("is-open");
+  if (refs.lightboxModal.classList.contains("is-open")) {
+    return refs.lightboxModal.classList.remove("is-open"),
+      refs.imgContentLightboxModal.removeAttribute('src'),
+      refs.imgContentLightboxModal.removeAttribute('alt');
+  }
+  return refs.lightboxModal.classList.add("is-open");
 }
 /*заменяем на упрощённую функцию */
 // function addClassForOpenModal() {
@@ -71,12 +71,12 @@ return refs.lightboxModal.classList.add("is-open");
 // }
 
 function addLinkOriginalImageInModal(linkImg) {
-    refs.imgContentLightboxModal.setAttribute('src', `${linkImg}`)
+  refs.imgContentLightboxModal.setAttribute('src', `${linkImg}`)
 }
 
 function removeLinkOriginalImageInModal() {
-    if (!refs.lightboxModal.classList.contains("is-open")) {
-        refs.imgContentLightboxModal.setAttribute('src', '')
-        // console.log('src-', refs.imgContentLightboxModal.attributes('src'))
-    }
+  if (!refs.lightboxModal.classList.contains("is-open")) {
+    refs.imgContentLightboxModal.setAttribute('src', '')
+    // console.log('src-', refs.imgContentLightboxModal.attributes('src'))
+  }
 }
