0699cc3bdcb1d68eee89bdaf5fd7b9bf0a2233c1
Art <Petrenkoartik@gmail.com>
2020-10-12 00:07:12 +0300

    comlete

--- js/data.js
+++ js/data.js
@@ -1,10 +1,17 @@
 import dataGalleryItems from './gallery-items.js'
-console.log(dataGalleryItems);
+// console.log(dataGalleryItems);
 
 const refs = {
-    listGalleryItems: document.querySelector('.js-gallery')
+    listGalleryItems: document.querySelector('.js-gallery'),
+    lightboxModal: document.querySelector('.js-lightbox'),
+    overlayLightboxModal: document.querySelector('.lightbox__overlay'),
+    contentLightboxModal: document.querySelector('.lightbox__content'),
+    btnLightboxModal: document.querySelector('.lightbox__button'),
+    imgContentLightboxModal: document.querySelector('.lightbox__image')
 }
-console.log(refs.listGalleryItems);
+// console.log(refs.listGalleryItems);
+// console.log(refs.imgContentLightboxModal);
+
 
 const cardsMarkupGalleryItems = createListGalleryItems(dataGalleryItems)
 refs.listGalleryItems.innerHTML = cardsMarkupGalleryItems;
@@ -28,4 +35,48 @@ function createListGalleryItems(items) {
     `;
     })
     .join('');
+}
+
+refs.listGalleryItems.addEventListener('click', viewOriginalImageGallery)
+
+function viewOriginalImageGallery(e) {e.preventDefault()
+    // console.log('целевой:', e.target);
+    // console.log('текущий:', e.currentTarget);
+     /* если не IMG - пропускаем */
+    if (e.target.nodeName !== 'IMG') { return }
+    const OriginalImageGallery = e.target.dataset.source
+    // console.log(OriginalImageGallery);
+
+    addClassForOpenModal()
+    addLinkOriginalImageInModal(OriginalImageGallery)
+    //  console.log('this:', refs.imgContentLightboxModal.getAttribute('src'));
+}
+
+refs.btnLightboxModal.addEventListener('click', addClassForOpenModal)
+refs.overlayLightboxModal.addEventListener('click', addClassForOpenModal)
+// refs.overlayLightboxModal.addEventListener('click', removeLinkOriginalImageInModal)
+
+function addClassForOpenModal(e) {
+    if (refs.lightboxModal.classList.contains("is-open")) {
+        return refs.lightboxModal.classList.remove("is-open"),
+         refs.imgContentLightboxModal.removeAttribute('src'),
+            refs.imgContentLightboxModal.removeAttribute('alt'),console.log( refs.imgContentLightboxModal.getAttribute('src'));
+
+    }
+return refs.lightboxModal.classList.add("is-open");
+}
+/*заменяем на упрощённую функцию */
+// function addClassForOpenModal() {
+//     refs.lightboxModal.classList.toggle("is-open")
+// }
+
+function addLinkOriginalImageInModal(linkImg) {
+    refs.imgContentLightboxModal.setAttribute('src', `${linkImg}`)
+}
+
+function removeLinkOriginalImageInModal() {
+    if (!refs.lightboxModal.classList.contains("is-open")) {
+        refs.imgContentLightboxModal.setAttribute('src', '')
+        // console.log('src-', refs.imgContentLightboxModal.attributes('src'))
+    }
 }
